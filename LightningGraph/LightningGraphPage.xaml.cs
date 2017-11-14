c90f4f4cc0dad5a85a02335c2618dad88d610bb3
Mathias Kirkegaard Nielsen <matn@bluefragments.com>
2017-11-14 19:40:05 +0100

    initial commit

---
+++ LightningGraph/LightningGraphPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace LightningGraph
+{
+    public partial class LightningGraphPage : ContentPage
+    {
+        public LightningGraphPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
