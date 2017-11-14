f961f1fab3a1493522538a2dba4bee64ae259e03
Mathias Kirkegaard Nielsen <matn@bluefragments.com>
2017-11-14 21:19:00 +0100

    Adding lightning effect

--- LightningGraph/LightningGraphPage.xaml.cs
+++ LightningGraph/LightningGraphPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace LightningGraph
 {
@@ -13,9 +14,29 @@ namespace LightningGraph
         {
             base.OnAppearing();
 
+            InitiateDataLoad();
+        }
+
+        private async void InitiateDataLoad()
+        {
+            await Task.Delay(1000);
+
+            InitiateFlash();
+
             var charts = DummyData.CreateXamarinSample();
             this.chart1.Chart = charts[0];
             this.chart2.Chart = charts[1];
+        }
+
+        private async void InitiateFlash()
+        {
+            FlashView.IsVisible = true;
+            FlashView.IsEnabled = false;
+            FlashView.Opacity = 1;
+
+            await FlashView.FadeTo(0, 1000, null);
+
+            Device.BeginInvokeOnMainThread(() => FlashView.IsVisible = false);
         }
     }
 }
